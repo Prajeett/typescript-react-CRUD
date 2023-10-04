9c8f761624fb049906d71e0056a0ec4d91547abf
Prajeet Basnet <prajitbasnet2000@gmail.com>
2023-10-04 21:47:28 +1000

    added more features on forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './components/model';
 
 
 
 
 const App: React.FC = () => {
+
+
+const [todo, setTodo] = useState <string>("");
+
+const [todos, setTodos] = useState <Todo[]>([])
+
+
+const handleAdd = (e:React.FormEvent) => {
+  e.preventDefault();
+}
+
+
+
+
   return (
     <div className="App">
       <span className='heading'>CURD app</span>
-      <InputField/>
+      <InputField todo={todo} setTodo = {setTodo} handleAdd = {handleAdd}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props{
+    todo:string, setTodo: React.Dispatch<React.SetStateAction<string>>, handleAdd: (e: React.FormEvent) => void;
+
+}
+
+
+
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
   return (
-    <form className='input'>
-        <input type="input" placeholder='Enter a data' className='input_box' />
+    <form className='input' onSubmit={handleAdd}>
+        <input type="input" placeholder='Enter a data' className='input_box' onChange={(e) => {
+           setTodo(e.target.value) ;
+        }} />
         <button className='input_submit' type='submit' >Go</button>
     </form>
   )
+  
 }
 
 export default InputField
